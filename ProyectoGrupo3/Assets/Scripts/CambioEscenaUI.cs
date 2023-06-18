c6e6ddc53689083b0687c22808329645b623b945
AlejandroPenarandaSoto <135558117+AlejandroPenarandaSoto@users.noreply.github.com>
2023-06-17 19:52:53 -0600

    Funcionalidad de los botones de UI

---
+++ ProyectoGrupo3/Assets/Scripts/CambioEscenaUI.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambioEscenaUI : MonoBehaviour
+{
+
+    //public TypeOfSound sonido = TypeOfSound.Boton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void changescene(string nombreEscena)
+    {
+
+
+        Debug.Log("Hola,has hecho click en este boton");
+        //Sound_Handler.Instance.playsound((int)sonido);
+        SceneManager.LoadScene(nombreEscena);
+    }
+    public void CambiarUltimaEscenaJugada()
+    {
+        SceneManager.LoadScene(PlayerPrefs.GetString("escenaActual", "Portada"));
+    }
+    public void ReseteoTiempo()
+    {
+        PlayerPrefs.SetFloat("TiempoEscena1", 0);
+        PlayerPrefs.SetFloat("TiempoEscena2", 0);
+        PlayerPrefs.SetFloat("TiempoEscena3", 0);
+
+    }
+    public void SalirJuego()
+    {
+        Application.Quit();
+    }
+}
